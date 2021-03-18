729117d2815146591c61126e5fafb35d9e6d47fe
Sahithi <sahithireddy6340@gmail.com>
2021-03-18 21:00:39 +0530

    initial commit

--- src/components/Images.js
+++ src/components/Images.js
@@ -5,14 +5,15 @@ function Images() {
     const [imageurl,setImgurl]=useState('')
     const [open,setOpen]=useState(false)
     const [imagearray,setImagearray]=useState([
-        "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        "https://assets.hongkiat.com/uploads/100-absolutely-beautiful-nature-wallpapers-for-your-desktop/blue-sea-sunset.jpg",
-        "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-         "https://image.shutterstock.com/image-photo/lovely-beach-picture-beautiful-260nw-1554809048.jpg",
-         "https://images.unsplash.com/photo-1527342726932-1d392fcdd316?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-         "https://thumbs.dreamstime.com/b/christmas-background-magic-glowing-snowflakes-winter-nature-landscape-beautiful-scene-bokeh-fairytale-illuminated-lights-154691551.jpg",
-         "https://cdn.wallpapersafari.com/63/79/ctZJYf.jpg",
-         "https://cdn.wallpapersafari.com/63/79/ctZJYf.jpg"
+        "https://i.picsum.photos/id/175/2896/1944.jpg?hmac=djMSfAvFgWLJ2J3cBulHUAb4yvsQk0d4m4xBJFKzZrs" ,
+        "https://i.picsum.photos/id/133/2742/1828.jpg?hmac=0X5o8bHUICkOIvZHtykCRL50Bjn1N8w1AvkenF7n93E",
+        "https://i.picsum.photos/id/21/3008/2008.jpg?hmac=T8DSVNvP-QldCew7WD4jj_S3mWwxZPqdF0CNPksSko4",
+        "https://i.picsum.photos/id/250/4928/3264.jpg?hmac=4oIwzXlpK4KU3wySTnATICCa4H6xwbSGifrxv7GafWU",
+        "https://i.picsum.photos/id/30/1280/901.jpg?hmac=A_hpFyEavMBB7Dsmmp53kPXKmatwM05MUDatlWSgATE",
+        "https://i.picsum.photos/id/464/3198/2230.jpg?hmac=PvWYTRxxxpPyuU2k_p7abQZAF584A6poZ0lH12q-k6w",
+        "https://i.picsum.photos/id/357/3888/2592.jpg?hmac=322FsZ93_k9v7NNFeCTlqk_gobPP_1mYJIQwk7GxjMc",
+        "https://i.picsum.photos/id/175/2896/1944.jpg?hmac=djMSfAvFgWLJ2J3cBulHUAb4yvsQk0d4m4xBJFKzZrs",
+        "https://i.picsum.photos/id/1059/7360/4912.jpg?hmac=vVWk1qyiXN_VgPhpNqFm3yl2HUPW6fHqYOjTHYO2bHQ"
 
     ])
     const [showmodal,setShowmodal]=useState(false)
@@ -21,10 +22,6 @@ function Images() {
     const images=imagearray.map((url,i)=>{
         return (
             <>
-            
-           
-            
-            
             </>
 
         ) 
@@ -60,20 +57,13 @@ function Images() {
                         <>
                   
                       <div className="col-md-4 col-sm-12 my-4 p-20">
-                      <div key={i} style={{height:'200px',width:'150px',display:'flex',padding:'20px'}}>
-            <div className="card" >
-  <img className="card-img-top" style={{height:'500px',width:"300px"}} src={url} alt="Card image cap" onClick={()=>{showImage(url)}}/>
+                          <div key={i} style={{height:'200px',width:'150px',display:'flex',padding:'20px'}}>
+                             <div className="card" >
+                                  <img className="card-img-top" style={{height:'500px',width:"300px"}} src={url} alt="Card image cap" onClick={()=>{showImage(url)}}/>
 
-                      </div>
-                  
-                      
-                            
-           
-
-   
-  
-</div>
-</div> 
+                                </div>
+                          </div>
+                     </div> 
             
             {open && (
           <dialog
